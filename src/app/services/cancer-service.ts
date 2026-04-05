a4934e2c03d2af51c29e71007ae7f051c593c07f
abhijeett55 <100180572+abhijeett55@users.noreply.github.com>
2026-04-06 01:45:56 +0530

    deployed

--- src/app/services/cancer-service.ts
+++ src/app/services/cancer-service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CancerService {
 
-  private apiUrl = 'http://localhost:5000/predict'; 
+  private apiUrl = 'http://127.0.0.1:5000/predict';
 
   constructor(private http: HttpClient) {}
 
